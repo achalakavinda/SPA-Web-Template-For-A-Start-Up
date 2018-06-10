ed5174d3d35a14f2f067fc1a9a65e9190d1f7f98
Achala Kavinda <achalakavinda25r@gmail.com>
2018-06-10 12:22:04 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	"use strict";
+
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
+
+
+	$(".fullscreen").css("height", window_height)
+	$(".fitscreen").css("height", fitscreen);
+
+  //-------- Active Sticky Js ----------//
+  $(".default-header").sticky({topSpacing:0});
+    
+});
